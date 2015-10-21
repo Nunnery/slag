34ec01e6a6bbadfa23c5680526f9b295f998cedd
Richard Harrah <topplethenun@tealcubegames.com>
2015-10-20 20:32:16 -0400

    updating slag_spec

--- spec/slag_spec.rb
+++ spec/slag_spec.rb
@@ -1,15 +1,12 @@
 RSpec.describe Slag do
+
   # Make sure that Slag::VERSION exists
-  describe '::VERSION' do
-    context 'when it exists' do
-      it { is_expected.to have_constant :VERSION }
-    end
-  end
+  it { is_expected.to have_constant 'VERSION' }
 
   # Make sure that Slag::ROOT exists
-  describe '::ROOT' do
-    context 'when it exists' do
-      it { is_expected.to have_constant :ROOT }
-    end
-  end
+  it { is_expected.to have_constant 'ROOT' }
+
+  # Make sure that Slag::FOOBAR does not exist
+  it { is_expected.to_not have_constant 'FOOBAR' }
+
 end
