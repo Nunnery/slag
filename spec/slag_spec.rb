e632aed4e048e835e73ca73575457df914f2ac6c
Richard Harrah <topplethenun@tealcubegames.com>
2015-10-20 20:19:23 -0400

    fixing documentation for slag_spec.rb

--- spec/slag_spec.rb
+++ spec/slag_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe Slag do
-  # Make sure that Mattock::VERSION exists
+  # Make sure that Slag::VERSION exists
   describe '::VERSION' do
     context 'when it exists' do
       it { is_expected.to have_constant :VERSION }
     end
   end
 
-  # Make sure that Mattock::ROOT exists
+  # Make sure that Slag::ROOT exists
   describe '::ROOT' do
     context 'when it exists' do
       it { is_expected.to have_constant :ROOT }
